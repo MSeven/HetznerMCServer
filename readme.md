e5d14e9966822d9458f3328ad24b3d69136e80f6
Martin Griesbach <mgriesbach@gmail.com>
2024-01-04 18:12:04 +0100

    switch from docker provider to cloud-init injected docker-compose, to enable startup in one action.

--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ Follow these steps:
 * Download Terraform from https://developer.hashicorp.com/terraform/install and place the executeable in this
   directory (or place it in your PATH).
 * Create a Account for Hetzner Cloud at https://accounts.hetzner.com/signUp
-* Create a New Project in the Hetzneer Cloud Ui (McServer)
+* Create a New Project in the Hetzner Cloud Ui (McServer)
 * In this project go to Security/API Token and create a new Token called 'Terraform' with Read&Write permissions.
 * Create a file named 'terraform.tfvars' with the content:
 
@@ -27,15 +27,12 @@ Open https://dns.hetzner.com/, go to the zone you want to control and import the
 
 > terraform import hetznerdns_zone.zone1 #id-from-url#
 
-Finally, Starting the server is a 2 step process. Simply run these 2 commands.
-
-> terraform apply --target=hcloud_server.minecraft
+Finally, Start the server with:
 
 > terraform apply
 
 To get the Ipaddress of the server run
 > terraform state show hcloud_server.minecraft
 
-
-Minecraft server sata is saved in an additional volume, which allows the server to be shut down/deleted (to save money)
-and recreated without losing the minecraft world. Note that this volume itself will incur a (minimal) cost over time.
+Minecraft server data is saved in an additional volume, which allows the server to be shut down/deleted and recreated
+without losing the minecraft world.
--- root.tf
+++ root.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "timohirt/hetznerdns"
       version = "2.2.0"
     }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
   }
 }
 
@@ -25,7 +21,6 @@ variable "hdns_token" {
 variable "hdns_domain" {
   sensitive = false
 }
-
 
 provider "hcloud" {
   token = var.hcloud_token
@@ -43,7 +38,6 @@ resource "hcloud_volume" "mcworld" {
   name              = "MC-world"
   size              = 10
   delete_protection = true
-  server_id         = hcloud_server.minecraft.id
   automount         = true
   format            = "ext4"
   labels            = {
@@ -56,12 +50,19 @@ resource "hetznerdns_zone" "zone1" {
   ttl  = 300
 }
 
-##Disable from here:
+data "template_file" "cloudinit" {
+  template = file("cloud-init.yml")
+
+  vars = {
+    docker-compose = base64gzip(file("docker-compose.yml"))
+    volume_path    = "/dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.mcworld.id}"
+  }
+}
 
 resource "hcloud_volume_attachment" "mcvolattach" {
   volume_id = hcloud_volume.mcworld.id
   server_id = hcloud_server.minecraft.id
-  automount = true
+  automount = false
 }
 
 resource "hcloud_server" "minecraft" {
@@ -79,33 +80,7 @@ resource "hcloud_server" "minecraft" {
   ssh_keys = [
     hcloud_ssh_key.mckey.name,
   ]
-}
-
-provider "docker" {
-  host     = "ssh://root@${hcloud_server.minecraft.ipv4_address}"
-  ssh_opts = ["-i", "./mckey"]
-}
-
-# Pulls the image
-resource "docker_image" "mc_docker" {
-  name = "itzg/minecraft-server"
-}
-
-# Create a container
-resource "docker_container" "mc_srv_1" {
-  image      = docker_image.mc_docker.image_id
-  name       = "mc_server_1"
-  tty        = true
-  stdin_open = true
-  ports {
-    internal = 25565
-    external = 25565
-  }
-  env = ["EULA=TRUE"]
-  volumes {
-    container_path = "/data"
-    host_path      = "/mnt/HC_Volume_${hcloud_volume.mcworld.id}"
-  }
+  user_data = data.template_file.cloudinit.rendered
 }
 
 resource "hetznerdns_record" "minecraft" {
